a2e37659a9435563cd37d923f9aeb0c306759ecb
gavin <gavin.deng@eyeseeuhd.com>
2015-04-14 20:20:46 +0800

    add 190_reverseBits with C and C++

---
+++ 190_ReverseBits/reverseBits.c
@@ -0,0 +1,58 @@
+/* ************************************************************************
+ *       Filename:  reverseBits.c
+ *    Description:  
+ *        Version:  1.0
+ *        Created:  2015Äê04ÔÂ14ÈÕ 19ʱ58·Ö12Ãë
+ *       Revision:  none
+ *       Compiler:  gcc
+ *         Author:  YOUR NAME (), 
+ *        Company:  
+ * ************************************************************************/
+/*  
+
+Reverse bits of a given 32 bits unsigned integer.
+
+For example, given input 43261596 (represented in binary as 00000010100101000001111010011100), return 964176192 (represented in binary as 00111001011110000010100101000000).
+
+Follow up:
+If this function is called many times, how would you optimize it?
+
+Related problem: Reverse Integer
+
+Credits:
+Special thanks to @ts for adding this problem and creating all test cases.
+*/
+
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+uint32_t reverseBits(uint32_t n) 
+{
+	int i = 0;
+	int num = 0;
+	for(i=0; i<32; i++){
+		num = num << 1;	
+		if(n&0x1){
+		//	num = num << 1;
+			num  = num | 0x1;
+		}
+		n = n >> 1;
+		
+	}
+	return num;	    
+}
+
+
+int main(int argc, char **argv)
+{
+	unsigned int num = 0;
+	if(argc > 1){
+		num = atoi(argv[1]);
+	}
+
+	printf("%u reverseBits is %u\n", num, reverseBits(num));
+
+	return 0;
+}
